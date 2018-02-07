0ca0652887553f2aafcfd68728e1ca69ff562cfd
ersanbr <ers2003@gmail.com>
2018-02-07 08:42:03 -0200

    Exportação Funcional

---
+++ src/view/dadosProcesso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author ersan
+ */
+public class dadosProcesso {
+
+    String acao;
+    String numeroProcesso;
+    String autor;
+    String reu;
+    String andamentos;
+
+    
+}
